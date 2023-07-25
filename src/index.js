6884d6d87d6010c3e101fbb77ba303b1dce9828f
47Billion <pooja.khandekar@47billion.com>
2023-07-25 11:30:21 +0530

    files inside src directory added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import CustomCursor from "./common/CustomCursor";
+import WhiteBoard from "./common/Whiteboard";
+import InputMapper from "./common/InputMapper";
+
+import App from "./App";
+
+ReactDOM.render(
+  <>
+    <CustomCursor></CustomCursor>
+    <WhiteBoard />
+    <InputMapper />
+    <App />
+  </>,
+  document.getElementById("root")
+);
